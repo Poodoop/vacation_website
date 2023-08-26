3b40b6c434daa3af55735b1c689731a0372da8be
Poodoop <dickyaristyo43@gmail.com>
2023-08-27 00:19:29 +0700

    change folder name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./dist/*.{html,js,jsx}"],
+  content: ["./docs/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
